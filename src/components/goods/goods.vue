7faacdc94b4fd96fec79d4a922be570807c78646
pine nut <627605616@qq.com>
2017-03-03 13:46:53 +0800

    完成header

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="111">
+  我是组件goods
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {};
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus">
+
+</style>
