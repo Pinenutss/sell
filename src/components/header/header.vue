87deb73cf5736e69bf8a1132b7bd75b16a91f753
Pinenut <627605616@qq.com>
2017-03-04 12:48:14 +0800

    完善header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,9 +13,30 @@
         {{seller.description}}/{{seller.deliveryTime}}分钟送达
       </div>
       <div class="support" v-if="seller.supports">
-        <span class="icon"></span>
+        <span class="icon" :class="classMap[seller.supports[0].type]"></span>
         <span class="text">{{seller.supports[0].description}}</span>
       </div>
+    </div>
+    <div class="support-count" v-if="seller.supports" @click="showDetail">
+      <span class="count">{{seller.supports.length}}个</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+  </div>
+  <div class="bulletin-wrapper" @click="showDetail">
+    <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+    <i class="icon-keyboard_arrow_right"></i>
+  </div>
+  <div class="background">
+    <img :src="seller.avatar" alt="" width="100%" height="100%">
+  </div>
+  <div v-show="detailShow" class="detail">
+    <div class="detail-wrapper clearfix">
+      <div class="detail-mian">
+
+      </div>
+    </div>
+    <div class="detail-close">
+      <i class="icon-close"></i>
     </div>
   </div>
 </div>
@@ -27,6 +48,19 @@ export default {
     seller: {
       type: Object
     }
+  },
+  data() {
+    return {
+      detailShow: false
+    };
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true;
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
   }
 };
 </script>
@@ -34,14 +68,19 @@ export default {
 <style rel="stylesheet/stylus" lang="stylus">
   @import "../../common/stylus/mixin.styl"
   .header
+    position: relative
+    overflow: hidden
     color:#fff
-    background:#000
+    background:rgba(7,17,27,.5)
     .content-wrapper
+      position:relative
       padding: 24px 12px 18px 24px
       font-size: 0
-      display: inline-block
       .avatar
         display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
       .content
         display: inline-block
         font-size: 14px
@@ -61,4 +100,102 @@ export default {
             font-size: 16px
             line-height: 18px
             font-weight: bold
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+         .support
+           .icon
+             display: inline-block
+             vertical-align: top
+             width: 12px
+             height: 12px
+             margin-right: 4px
+             background-size: 12px 12px
+             background-repeat: no-repeat
+             &.decrease
+              bg-image('decrease_1')
+             &.discount
+              bg-image('discount_1')
+             &.guarantee
+              bg-image('guarantee_1')
+             &.invoice
+              bg-image('invoice_1')
+             &.special
+              bg-image('special_1')
+          .text
+            line-height: 12px
+            font-size: 10px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0,0,0,0.2)
+        .count
+          vertical-align: top
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height: 24px
+          font-size: 10px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(1,17,27,0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 7px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        font-size: 10px
+        margin: 0 4px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7,17,27,.8)
+      .detail-wrapper
+        min-height: 100%
+        .detail-mian
+          margin-top: 64px
+          padding-bottom: 64px
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
